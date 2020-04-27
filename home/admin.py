5a8672d244c3685c4af8e0e078009376cbcb42a6
metehankafadar <metehankafadar@gmail.com>
2020-04-27 20:50:51 +0300

    13. video bitti

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from home.models import Setting
+from home.models import Setting,ContactFormMessage
+
+class ContactformMessageAdmin(admin.ModelAdmin):
+    list_display = ['name','email','subject','message','note','status']
+    list_filter = ['status']
+
+admin.site.register(ContactFormMessage,ContactformMessageAdmin)
 
 admin.site.register(Setting)
--- home/models.py
+++ home/models.py
@@ -1,4 +1,8 @@
+from ckeditor_uploader.fields import RichTextUploadingField
 from django.db import models
+
+from django.forms import ModelForm, Textarea, TextInput
+
 
 # Create your models here.
 class Setting(models.Model):
@@ -22,13 +26,42 @@ class Setting(models.Model):
     facebook = models.CharField(blank=True,max_length=50)
     instagram = models.CharField(blank=True,max_length=50)
     twitter = models.CharField(blank=True,max_length=50)
-    aboutus = models.TextField(blank=True)
-    referances = models.TextField(blank=True)
-    contact = models.TextField(blank=True)
+    aboutus = RichTextUploadingField(blank=True)
+    referances = RichTextUploadingField(blank=True)
+    contact = RichTextUploadingField(blank=True)
     status=models.CharField(max_length=10,choices=STATUS)
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now_add=True)
 
-
     def __str__(self):
         return self.title
+
+class ContactFormMessage(models.Model):
+    STATUS=(
+        ('New','New'),
+        ('Read','Read'),
+    )
+    name =models.CharField(blank=True,max_length=50)
+    email=models.CharField(blank=True,max_length=50)
+    subject=models.CharField(blank=True,max_length=50)
+    message=models.CharField(blank=True,max_length=255)
+    status=models.CharField(max_length=10,choices=STATUS,default='New')
+    ip=models.CharField(blank=True,max_length=20)
+    note=models.CharField(blank=True,max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ContactFormu(ModelForm):
+    class Meta:
+        model=ContactFormMessage
+        fields = ['name','email','subject','message']
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control','placeholder':'Name & Surname','required':"required"}),
+            'subject': TextInput(attrs={'class': 'form-control', 'placeholder': 'subject','required':"required"}),
+            'email': TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Adress','required':"required"}),
+            'message': Textarea(attrs={'class': 'form-control', 'placeholder': 'Your Message','rows':'10' ,'required':"required"}),
+        }

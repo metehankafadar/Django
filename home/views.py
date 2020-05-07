c4c28b5aa7f5492f5b875597ce2be00d61cc2ee2
metehankafadar <metehankafadar@gmail.com>
2020-05-07 18:01:33 +0300

    21.video bitti / oto tamamlama da sıkıntı var. Bakılacak tekrar

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+import json
 from pyexpat.errors import messages
 
 from django.http import HttpResponse, HttpResponseRedirect
@@ -86,13 +87,35 @@ def product_search(request):
         if form.is_valid():
             category = Category.objects.all()
             query = form.cleaned_data['query']
-            products = Product.objects.filter(title__icontains=query)
+            catid = form.cleaned_data['catid']
+
+            if catid == 0:
+                products = Product.objects.filter(title__icontains=query)
+            else:
+                products = Product.objects.filter(title__icontains=query, category_id=catid)
+
+
+
             context = {'products': products,
                        'category': category,
-                       'query': query,
                        }
-            return render(request,'products_search.html', context)
+            return render(request, 'products_search.html', context)
 
     return HttpResponseRedirect('/')
+
+def product_search_auto(request):
+  if request.is_ajax():
+    q = request.GET.get('term', '')
+    product = Product.objects.filter(title__icontains=q)
+    results = []
+    for rs in product:
+      product_json = {}
+      product_json = rs.title
+      results.append(product_json)
+    data = json.dumps(results)
+  else:
+    data = 'fail'
+  mimetype = 'application/json'
+  return HttpResponse(data, mimetype)
 
 

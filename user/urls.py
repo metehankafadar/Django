9784ecda98e1ac6c01027a730850cecc08fcf57d
metehankafadar <metehankafadar@gmail.com>
2020-05-16 19:54:19 +0300

    33.video bitti

--- user/urls.py
+++ user/urls.py
@@ -8,7 +8,9 @@ urlpatterns = [
     path('update/',views.user_update,name='user_update'),
     path('password/',views.change_password,name='change_password'),
     path('orders/',views.orders,name='orders'),
-    path('orderdetail/<int:id>', views.orderdetail, name='orderdetail')
+    path('orderdetail/<int:id>', views.orderdetail, name='orderdetail'),
+    path('comments/',views.comments,name='comments'),
+    path('deletecomment/<int:id>', views.deletecomment, name='deletecomment'),
     #path('addcomment/<int:id>', views.addComment, name='addcomment')
 
 
